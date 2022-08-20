d4ecffe5cf561660754b9263573f639d04f5a77b
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-08-20 21:46:55 +0300

    Assignment Complited

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries;
+
+function reducer(){
+
+totalBatteries=batteryBatches.reduce(arraySummation,0)
+
+//Debug :console.log(totalBatteries);
+
+return totalBatteries;
+}
+
+const arraySummation =(sum,currentValue)=>{
+//Debug :console.log(`Sum is ${sum} and current value is ${currentValue}`);
+sum += currentValue;
+
+return sum;
+
+}
+reducer();
